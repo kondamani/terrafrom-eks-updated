d6c0fd250c3dd343b350e0bb54d2ea0499d08703
kondamani <m.kondamani1985@gmail.com>
2025-07-23 21:01:44 +0530

    changes in variables and provider for awsregion

--- provider.tf
+++ provider.tf
@@ -9,11 +9,4 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks_cluster_name.cluster_certificate_authority_data)
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks_cluster_name.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks_cluster_name.cluster_name
-}
 
