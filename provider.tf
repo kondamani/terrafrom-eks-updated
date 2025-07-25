4a9e94063a09d25aee34e00206a59021ace7a43a
kondamani <m.kondamani1985@gmail.com>
2025-07-25 12:48:39 +0530

    creating new eks commented s3 and dnamodb

--- provider.tf
+++ provider.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
+  host                   = module.eks_cluster_name.cluster_endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks_cluster_name.cluster_name
-}
+#data "aws_eks_cluster_auth" "cluster" {
+  #name = module.eks_cluster_name.cluster_name
+#}
 
 data "aws_eks_cluster" "eks" {
   name = module.eks_cluster_name.cluster_name
